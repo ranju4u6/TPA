bd4cc9c896ec3c133c87a8dfc109f0dcf5b298d7
ranju4u6 <ranju4u6@gmail.com>
2018-08-31 08:37:26 +0530

    initial commit

---
+++ TPAWebPortal/backend/src/main/java/com/allstate/tpa/portal/TPAWebPortal/TpaWebPortalApplication.java
@@ -0,0 +1,12 @@
+package com.allstate.tpa.portal.TPAWebPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpaWebPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpaWebPortalApplication.class, args);
+	}
+}
